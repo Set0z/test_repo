ef2b7835a3f86c4f8c0e3c7e43b118735f33a5e0
Setoz <markiss226@gmail.com>
2025-04-10 13:21:02 +0300

    Add files via upload

---
+++ da.ps1
@@ -0,0 +1,9 @@
+﻿#
+Get-ExecutionPolicy
+Get-ExecutionPolicy -List
+pause
+if(($(Get-ExecutionPolicy) -eq "Restricted") -or ($(Get-ExecutionPolicy) -eq "AllSigned")) {
+        Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/Set0z/Buran_Menu/refs/heads/main/modules/script.ps1 | iex"' -Verb RunAs
+        exit
+}
+pause
